05f8cf352b4852b033d6368c6ad689b2eadd7cfc
sh <16303791+speedyhoon@users.noreply.github.com>
2018-03-29 16:06:49 +1100

    Replace value fields with getters

--- forms.go
+++ forms.go
@@ -9,19 +9,46 @@ type Form struct {
 }
 
 type Field struct {
-	Name, Error, Value, Placeholder       string
-	Options                               []Option
-	Max, Min, MaxLen, MinLen              int
-	Step, ValueFloat32                    float32
-	Regex                                 *regexp.Regexp
-	V8                                    func(*Field, ...string)
-	ValueUint                             uint
-	ValueUintSlice                        []uint
-	Required, Disable, AutoFocus, Checked bool
-	//size                                  uint8
+	Name, Err, Placeholder   string
+	Options                  []Option
+	Max, Min, MaxLen, MinLen int
+	Step                     float32
+	Regex                    *regexp.Regexp
+	V8                       func(*Field, ...string)
+	Value                    interface{}
+	Required, Disable, Focus bool
+	//Size                     uint8
 }
 
 type Option struct {
 	Label, Value string
 	Selected     bool
+}
+
+func (f *Field)Str()string{
+	return f.Value.(string)
+}
+
+func (f *Field)StrSlice()[]string{
+	return f.Value.([]string)
+}
+
+func (f *Field)Float()float32{
+	return f.Value.(float32)
+}
+
+func (f *Field)Float64()float64{
+	return f.Value.(float64)
+}
+
+func (f *Field)Uint()uint{
+	return f.Value.(uint)
+}
+
+func (f *Field)UintSlice()[]uint{
+	return f.Value.([]uint)
+}
+
+func (f *Field)Checked()bool{
+	return f.Value.(bool)
 }
