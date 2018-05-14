14d77136096fd46615eba3f9bd34a76ef77e186a
sh <16303791+speedyhoon@users.noreply.github.com>
2018-05-14 16:02:57 +1000

    gofmt

--- forms.go
+++ forms.go
@@ -32,41 +32,41 @@ type Option struct {
 }
 
 //Str returns f.Value as a string.
-func (f *Field)Str()string{
+func (f *Field) Str() string {
 	return f.Value.(string)
 }
 
 //StrSlice returns f.Value as a string slice.
-func (f *Field)StrSlice()[]string{
+func (f *Field) StrSlice() []string {
 	return f.Value.([]string)
 }
 
 //Float returns f.Value as a float32.
-func (f *Field)Float()float32{
+func (f *Field) Float() float32 {
 	return f.Value.(float32)
 }
 
 //Float64 returns f.Value as a float64.
-func (f *Field)Float64()float64{
+func (f *Field) Float64() float64 {
 	return f.Value.(float64)
 }
 
 //Uint returns f.Value as an unsigned integer.
-func (f *Field)Uint()uint{
+func (f *Field) Uint() uint {
 	return f.Value.(uint)
 }
 
 //UintSlice returns f.Value as an unsigned integer slice.
-func (f *Field)UintSlice()[]uint{
+func (f *Field) UintSlice() []uint {
 	return f.Value.([]uint)
 }
 
 //Checked returns f.Value as a boolean.
-func (f *Field)Checked()bool{
+func (f *Field) Checked() bool {
 	return f.Value.(bool)
 }
 
 //Time returns f.Value as a datetime.
-func (f *Field)Time()time.Time{
+func (f *Field) Time() time.Time {
 	return f.Value.(time.Time)
 }
