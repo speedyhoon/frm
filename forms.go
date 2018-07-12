a4bdf319852fc8d1c44beef7e4cc9767e9ffc36d
sh <16303791+speedyhoon@users.noreply.github.com>
2018-07-12 15:03:18 +1000

    Fix references to nil pointers & nil interfaces

--- forms.go
+++ forms.go
@@ -32,41 +32,65 @@ type Option struct {
 }
 
 //Str returns f.Value as a string.
-func (f *Field) Str() string {
+func (f *Field) Str() (o string) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.(string)
 }
 
 //StrSlice returns f.Value as a string slice.
-func (f *Field) StrSlice() []string {
+func (f *Field) StrSlice() (o []string) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.([]string)
 }
 
 //Float returns f.Value as a float32.
-func (f *Field) Float() float32 {
+func (f *Field) Float() (o float32) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.(float32)
 }
 
 //Float64 returns f.Value as a float64.
-func (f *Field) Float64() float64 {
+func (f *Field) Float64() (o float64) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.(float64)
 }
 
 //Uint returns f.Value as an unsigned integer.
-func (f *Field) Uint() uint {
+func (f *Field) Uint() (o uint) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.(uint)
 }
 
 //UintSlice returns f.Value as an unsigned integer slice.
-func (f *Field) UintSlice() []uint {
+func (f *Field) UintSlice() (o []uint) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.([]uint)
 }
 
 //Checked returns f.Value as a boolean.
-func (f *Field) Checked() bool {
+func (f *Field) Checked() (o bool) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.(bool)
 }
 
 //Time returns f.Value as a datetime.
-func (f *Field) Time() time.Time {
+func (f *Field) Time() (o time.Time) {
+	if f == nil || f.Value == nil {
+		return
+	}
 	return f.Value.(time.Time)
 }
